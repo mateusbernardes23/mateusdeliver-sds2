2afe0425ac10dff71b582dc2fb948cd5c72c6665
Mateus Augusto <mateusbernardes23@hotmail.com>
2021-01-04 20:53:26 -0300

    Project Created

---
+++ backend/src/main/java/com/mateus/devsuperior/mateusdeliver/MateusdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.devsuperior.mateusdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateusdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateusdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mateus/devsuperior/mateusdeliver/MateusdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.devsuperior.mateusdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateusdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
